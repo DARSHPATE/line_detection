15a9efa1347bcf2b4c92987f59a920898b0e6d13
Darsh Patel <113710515+DARSHPATE@users.noreply.github.com>
2025-02-18 08:45:00 -0500

    Update joever.py

--- joever.py
+++ joever.py
@@ -2,30 +2,27 @@ import cv2
 import numpy as np
 
 def create_mask(image):
-    #do an roi
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower_yellow = np.array([32, 25, 80])
-    upper_yellow = np.array([80, 255, 255])
-    mask = cv2.bitwise_not(cv2.inRange(hsv, lower_yellow, upper_yellow))
+    x_margin_top = 900
+    x_margin_bottom = 225
+    y_margin_top = 150
+    y_margin_bottom = 500
+    offset = 100
+    vertices = np.array([[(x_margin_top, y_margin_top), (frame.shape[1] - x_margin_top + offset, y_margin_top), (frame.shape[1] - x_margin_bottom + offset, frame.shape[0] - y_margin_bottom), (x_margin_bottom, frame.shape[0] - y_margin_bottom)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, vertices, (255, 255, 255))
+    roi = cv2.bitwise_and(image, mask)
+
+    hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    lower_yellow = np.array([20, 30, 0])
+    upper_yellow = np.array([30, 100, 255])
+
+    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    mask = cv2.bitwise_not(mask)
     kernel = np.ones((3, 3), np.uint8)
-    mask = cv2.dilate(mask, kernel, iterations=2)
-    mask = cv2.erode(mask, kernel, iterations=2)
-    result = cv2.bitwise_and(image, image, mask=mask)
-    ratio = 1.0 / 3
-    for i in range(2):
-        if i == 0:
-            side = result[:, :int(ratio * result.shape[1])]
-        else:
-            side = result[:, int((1 - ratio) * result.shape[1]):]
-        for slice in side:
-            zeroes = np.where(slice == (0, 0, 0))[0]
-            if len(zeroes) > 0:
-                if i == 0:
-                    last_zero = zeroes[-1]
-                    slice[:last_zero] = [0, 0, 0]
-                else:
-                    first_zero = zeroes[0]
-                    slice[first_zero:] = [0, 0, 0]
+    mask = cv2.erode(mask, kernel, iterations=3)
+    mask = cv2.dilate(mask, kernel, iterations=3)=9
+    result = cv2.bitwise_and(roi, roi, mask=mask)
+
     return result
 
 cap = cv2.VideoCapture('driving.mov')
